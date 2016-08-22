a4c1b92d692d53270cdb63e42e0f94064b873f11
Roberto Frenna <roberto.frenna@gmail.com>
2016-08-23 01:04:39 +0200

    BotAPI.pm: version bump to 0.07

--- lib/WWW/Telegram/BotAPI.pm
+++ lib/WWW/Telegram/BotAPI.pm
@@ -7,7 +7,7 @@ use Encode ();
 use JSON::MaybeXS ();
 use constant DEBUG => $ENV{TELEGRAM_BOTAPI_DEBUG} || 0;
 
-our $VERSION = "0.06";
+our $VERSION = "0.07";
 my $json; # for debugging purposes, only defined when DEBUG = 1
 
 BEGIN {
