ae9217404371674481fb13a8940d6fd4ae2b8ba6
Roberto Frenna <roberto.frenna@gmail.com>
2018-02-05 00:48:42 +0100

    BotAPI: wrap calls to encode_json() with parentheses
    This avoids obscure Perl precedence issues, which should have been caught a long time ago. Fixes #21, thanks Flavio for the great issue reporting!

--- lib/WWW/Telegram/BotAPI.pm
+++ lib/WWW/Telegram/BotAPI.pm
@@ -152,7 +152,7 @@ sub api_request
             }
             else
             {
-                $postdata->{$k} = JSON::MaybeXS::encode_json $postdata->{$k}, next
+                $postdata->{$k} = JSON::MaybeXS::encode_json ($postdata->{$k}), next
                     if $has_file_upload;
                 push @fixable_keys, $k;
             }
@@ -160,7 +160,7 @@ sub api_request
         if ($has_file_upload)
         {
             # Fix keys found before the file upload.
-            $postdata->{$_} = JSON::MaybeXS::encode_json $postdata->{$_} for @fixable_keys;
+            $postdata->{$_} = JSON::MaybeXS::encode_json ($postdata->{$_}) for @fixable_keys;
             $postdata->{$_} = Encode::encode ("utf-8", $postdata->{$_})  for @utf8_keys;
             $is_lwp
                 and push @request, Content      => $postdata,
@@ -171,7 +171,7 @@ sub api_request
         {
             $is_lwp
                 and push @request, DEBUG ? (DBG => $postdata) : (), # handled in _fix_request_args
-                                   Content      => JSON::MaybeXS::encode_json $postdata,
+                                   Content      => JSON::MaybeXS::encode_json ($postdata),
                                    Content_Type => "application/json"
                 or  push @request, json         => $postdata;
         }
